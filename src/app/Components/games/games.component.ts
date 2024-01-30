85ccfada8caacae2bec6d233492e90df94c545a6
Armando Isaac García Del Rincón <agarcia@polyworksmexico.com>
2024-01-30 11:20:13 -0600

    Refactored games into interface

--- src/app/Components/games/games.component.ts
+++ src/app/Components/games/games.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Game } from '../../Interfaces/game';
 
 @Component({
   selector: 'app-games',
@@ -22,9 +23,15 @@ export class GamesComponent {
     this.addFavoriteEvent.emit(gameName);
   }
 
-  games = [
+  games: Game[] = [
     { id: 1, name: 'Uncharted' },
     { id: 2, name: 'Halo' },
     { id: 3, name: 'Monster Hunter' },
+    { id: 4, name: 'Dark Souls' },
+    { id: 4, name: 'Bloodborne' },
+    { id: 6, name: 'Overcooked' },
+    { id: 7, name: 'The Legend of Zelda' },
+    { id: 8, name: 'Gears of War' },
+    { id: 9, name: 'The Witcher' },
   ];
 }
---
+++ src/app/Interfaces/game.ts
@@ -0,0 +1,7 @@
+export interface Game {
+  id: number;
+  name: string;
+  platform?: string[];
+  photo?: string;
+  multiplayer?: boolean;
+}
